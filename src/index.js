d2dc87697056d62dae2c28a015e00a14adb25e3e
Neeraj Singh <869neerajsingh@gmail.com>
2024-05-02 17:47:02 +0530

    feat: create context provider for firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+import { FirebaseContext } from './context/firebase';
+import { app, db } from './configs/firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <FirebaseContext.Provider value={{ app, db }}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </FirebaseContext.Provider>
 );
